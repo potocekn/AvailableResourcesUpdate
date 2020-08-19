6b3dabafb9d6046c2f1356cda712287bbc5b09c5
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-19 14:36:40 +0200

    added class Page and its short description (might need some addition later)

--- AvailableResourceUpdate/Page.cs
+++ AvailableResourceUpdate/Page.cs
@@ -6,6 +6,7 @@ namespace AvailableResourceUpdate
 {
     /// <summary>
     /// This class represents web page. It stores information about page hash, file name for file with its content and url of the page.
+    /// Class is used for comparing contents of pages in server and in local database.
     /// </summary>
     class Page
     {
