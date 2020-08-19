6e3f1925171bd81a925e48a735fe0441a5c0bf42
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-19 14:31:23 +0200

    Add project files.

---
+++ AvailableResourceUpdate/Page.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailableResourceUpdate
+{
+    /// <summary>
+    /// This class represents web page. It stores information about page hash, file name for file with its content and url of the page.
+    /// </summary>
+    class Page
+    {
+        public string Hash { get; set; }
+        public string FileName { get; set; }
+        public string Url { get; set; }
+        public Page(string hash, string fileName, string url)
+        {
+            this.Hash = hash;
+            this.FileName = fileName;
+            this.Url = url;
+        }
+    }
+}
