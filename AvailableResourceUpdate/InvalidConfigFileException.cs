f60c40bc5a0694d8e4cd8c8ee9a8e296f3d67a97
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-19 15:43:43 +0200

    added InvalidConfigFileException for checking if config file exists

---
+++ AvailableResourceUpdate/InvalidConfigFileException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailableResourceUpdate
+{
+    /// <summary>
+    /// This exception is thrown when there is invalid configuration file.
+    /// </summary>
+    class InvalidConfigFileException : Exception
+    {
+        public InvalidConfigFileException() { }
+
+        public InvalidConfigFileException(string name)
+        : base(String.Format("Config file: {0} DOES NOT EXIST!", name)){ }
+    }
+}
