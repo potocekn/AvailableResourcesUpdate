19d2cb10ab9665d76404a38add413a9e93192521
Natalia Potocekova <natalia.potocekova@yahoo.com>
2020-08-19 16:16:49 +0200

    added InvalidDbsFileException

---
+++ AvailableResourceUpdate/InvalidDbsFileException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AvailableResourceUpdate
+{
+    class InvalidDbsFileException : Exception
+    {
+        public InvalidDbsFileException() { }
+
+        public InvalidDbsFileException(string path, string name)
+        : base(String.Format("Dbs file: {0}{1} DOES NOT EXIST!", path, name)) { }
+    }
+}
